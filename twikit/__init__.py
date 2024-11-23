d8e8c444da81bbfe0b8fac3baa978521472d4371
valeriisotnyk <v.sotnik@temida.io>
2024-11-23 21:26:31 +0100

    updates

--- twikit/__init__.py
+++ twikit/__init__.py
@@ -3,28 +3,29 @@
 Twikit Twitter API Wrapper
 ==========================
 
+https://github.com/d60/twikit
 A Python library for interacting with the Twitter API.
 """
 
-from .client import Client
+__version__ = '2.1.3'
+
+import asyncio
+import os
+
+if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+from ._captcha import Capsolver
+from .bookmark import BookmarkFolder
+from .errors import *
+from .utils import build_query
+from .client.client import Client
+from .community import Community, CommunityCreator, CommunityMember, CommunityRule
+from .geo import Place
 from .group import Group, GroupMessage
 from .list import List
-from .errors import (
-    BadRequest,
-    TwoFactorChallenge,
-    LoginAcid,
-    Forbidden,
-    NotFound,
-    RequestTimeout,
-    ServerError,
-    TooManyRequests,
-    TwitterException,
-    Unauthorized
-)
 from .message import Message
+from .notification import Notification
 from .trend import Trend
-from .tweet import ScheduledTweet, Tweet
+from .tweet import CommunityNote, Poll, ScheduledTweet, Tweet
 from .user import User
-from .utils import build_query
-
-__version__ = '1.3.15'
--- twikit/errors.py
+++ twikit/errors.py
@@ -1,80 +1,72 @@
+from __future__ import annotations
+
+
 class TwitterException(Exception):
     """
     Base class for Twitter API related exceptions.
     """
-
-
-class TwoFactorChallenge(Exception):
-    """
-    Exception raised for LoginTwoFactorAuthChallenge.
-    """
-
-
-class LoginAcid(Exception):
-    """
-    Exception raised for LoginAcid.
-    """
-
+    def __init__(self, *args: object, headers: dict | None = None) -> None:
+        super().__init__(*args)
+        if headers is None:
+            self.headers = None
+        else:
+            self.headers = dict(headers)
 
 class BadRequest(TwitterException):
     """
     Exception raised for 400 Bad Request errors.
     """
 
-
 class Unauthorized(TwitterException):
     """
     Exception raised for 401 Unauthorized errors.
     """
-
 
 class Forbidden(TwitterException):
     """
     Exception raised for 403 Forbidden errors.
     """
 
-
 class NotFound(TwitterException):
     """
     Exception raised for 404 Not Found errors.
     """
-
 
 class RequestTimeout(TwitterException):
     """
     Exception raised for 408 Request Timeout errors.
     """
 
-
 class TooManyRequests(TwitterException):
     """
     Exception raised for 429 Too Many Requests errors.
     """
-
+    def __init__(self, *args, headers: dict | None = None) -> None:
+        super().__init__(*args, headers=headers)
+        if headers is not None and 'x-rate-limit-reset' in headers:
+            self.rate_limit_reset = int(headers.get('x-rate-limit-reset'))
+        else:
+            self.rate_limit_reset = None
 
 class ServerError(TwitterException):
     """
     Exception raised for 5xx Server Error responses.
     """
 
-
 class CouldNotTweet(TwitterException):
     """
     Exception raised when a tweet could not be sent.
     """
-
 
 class DuplicateTweet(CouldNotTweet):
     """
     Exception raised when a tweet is a duplicate of another.
     """
 
-
 class TweetNotAvailable(TwitterException):
     """
     Exceptions raised when a tweet is not available.
     """
-
 
 class InvalidMedia(TwitterException):
     """
@@ -82,6 +74,25 @@ class InvalidMedia(TwitterException):
     sent with the tweet.
     """
 
+class UserNotFound(TwitterException):
+    """
+    Exception raised when a user does not exsit.
+    """
+
+class UserUnavailable(TwitterException):
+    """
+    Exception raised when a user is unavailable.
+    """
+
+class AccountSuspended(TwitterException):
+    """
+    Exception raised when the account is suspended.
+    """
+
+class AccountLocked(TwitterException):
+    """
+    Exception raised when the account is locked (very likey is Arkose challenge).
+    """
 
 ERROR_CODE_TO_EXCEPTION: dict[int, TwitterException] = {
     187: DuplicateTweet,
